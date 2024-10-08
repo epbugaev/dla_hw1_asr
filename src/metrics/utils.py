520ff04f6e78bd6da2deb48546a6812f7cc248e3
Бугаев Егор <epbugaev@Bugaevs-MacBook-Pro.local>
2024-10-08 17:30:06 +0300

    Implement utils.py

--- src/metrics/utils.py
+++ src/metrics/utils.py
@@ -1,13 +1,19 @@
-# Based on seminar materials
+import editdistance
 
+# Based on seminar materials
 # Don't forget to support cases when target_text == ''
 
 
 def calc_cer(target_text, predicted_text) -> float:
-    # TODO
-    pass
-
+    # DONE
+    if len(target_text) == 0:
+        return 1.0
+    
+    return editdistance.eval(target_text.split(), predicted_text.split()) / len(target_text.split())
 
 def calc_wer(target_text, predicted_text) -> float:
-    # TODO
-    pass
+    # DONE
+    if len(target_text) == 0:
+        return 1.0
+    
+    return editdistance.eval(target_text, predicted_text) / len(target_text.split())
