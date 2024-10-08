ce828b9cfc1846dc9410fdf3b9604db9cc708f18
Бугаев Егор <epbugaev@Bugaevs-MacBook-Pro.local>
2024-10-08 17:29:47 +0300

    Implement collate_fn

--- src/datasets/collate.py
+++ src/datasets/collate.py
@@ -1,5 +1,5 @@
 import torch
-
+import torch.nn.functional as F
 
 def collate_fn(dataset_items: list[dict]):
     """
@@ -14,4 +14,16 @@ def collate_fn(dataset_items: list[dict]):
             of the tensors.
     """
 
-    pass  # TODO
+    # DONE
+    result_batch = {}
+    result_batch['text'] = [item['text'] for item in dataset_items]
+    result_batch['audio_path'] = [item['audio_path'] for item in dataset_items]
+
+    for k in ['audio', 'text_encoded', 'spectrogram']:
+        last_dim_mx = max([item[k].shape[-1] for item in dataset_items])
+
+        # Pad all tensors so last dimension sizes match
+        result_batch[k] = torch.concat([F.pad(item[k], (0, last_dim_mx - item[k].shape[-1]), "constant", 0) for item in dataset_items], axis=0)
+        result_batch[k + '_length'] = torch.tensor([item[k].shape[-1] for item in dataset_items])
+
+    return result_batch
