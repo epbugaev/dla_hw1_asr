3db16672a427cfd6fb349ab97563d6279c45fd82
Бугаев Егор <epbugaev@Bugaevs-MacBook-Pro.local>
2024-10-08 18:42:00 +0300

    Fix utils

--- src/metrics/utils.py
+++ src/metrics/utils.py
@@ -9,11 +9,11 @@ def calc_cer(target_text, predicted_text) -> float:
     if len(target_text) == 0:
         return 1.0
     
-    return editdistance.eval(target_text.split(), predicted_text.split()) / len(target_text)
+    return editdistance.eval(target_text, predicted_text) / len(target_text)
 
 def calc_wer(target_text, predicted_text) -> float:
     # DONE
     if len(target_text) == 0:
         return 1.0
     
-    return editdistance.eval(target_text, predicted_text) / len(target_text.split())
+    return editdistance.eval(target_text.split(), predicted_text.split()) / len(target_text.split())
